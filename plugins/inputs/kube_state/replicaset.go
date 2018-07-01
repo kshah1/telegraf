82644203cdaf5509b6f5c839ee1ac01b2a042210
Kelvin Wang <sherkrainwang@gmail.com>
2018-07-01 12:17:31 -0700

    add kube state metrics

---
+++ plugins/inputs/kube_state/replicaset.go
@@ -0,0 +1,64 @@
+package kube_state
+
+import (
+	"context"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"k8s.io/api/apps/v1beta2"
+)
+
+var (
+	replicaSetMeasurement       = "kube_replicasets"
+	replicaSetStatusMeasurement = "kube_replicasets_status"
+)
+
+func registerReplicaSetCollector(ctx context.Context, acc telegraf.Accumulator, ks *KubenetesState) {
+	list, err := ks.client.getReplicaSets(ctx)
+	if err != nil {
+		acc.AddError(err)
+		return
+	}
+	for _, d := range list.Items {
+		if err = ks.gatherReplicaSet(d, acc); err != nil {
+			acc.AddError(err)
+			return
+		}
+	}
+}
+
+func (ks *KubenetesState) gatherReplicaSet(d v1beta2.ReplicaSet, acc telegraf.Accumulator) error {
+	var creationTime time.Time
+	if !d.CreationTimestamp.IsZero() {
+		creationTime = d.CreationTimestamp.Time
+	}
+	fields := map[string]interface{}{
+		"metadata_generation": d.ObjectMeta.Generation,
+	}
+	tags := map[string]string{
+		"namespace":  d.Namespace,
+		"replicaset": d.Name,
+	}
+	if d.Spec.Replicas != nil {
+		fields["spec_replicas"] = *d.Spec.Replicas
+
+	}
+	acc.AddFields(replicaSetMeasurement, fields, tags, creationTime)
+	return ks.gatherReplicaSetStatus(d, acc)
+}
+
+func (ks *KubenetesState) gatherReplicaSetStatus(d v1beta2.ReplicaSet, acc telegraf.Accumulator) error {
+	status := d.Status
+	fields := map[string]interface{}{
+		"replicas":               status.Replicas,
+		"fully_labeled_replicas": status.FullyLabeledReplicas,
+		"ready_replicas":         status.ReadyReplicas,
+		"observed_generation":    status.ObservedGeneration,
+	}
+	tags := map[string]string{
+		"namespace":  d.Namespace,
+		"replicaset": d.Name,
+	}
+	acc.AddFields(replicaSetStatusMeasurement, fields, tags)
+	return nil
+}
